b1f8f77677831c5e82a9157724af3883aee502d8
amanm007 <95200325+amanm007@users.noreply.github.com>
2024-03-13 19:02:53 -0700

    Slow Mo

--- Slow and Steady/Assets/SlowMotionAbility.cs
+++ Slow and Steady/Assets/SlowMotionAbility.cs
@@ -2,22 +2,33 @@ using UnityEngine;
 
 public class SlowMotionAbility : MonoBehaviour
 {
-    public float slowMotionFactor = 0.2f; 
-    public float slowMotionDuration = 5f; // Duration of the slow-motion effect
+    public float slowMotionFactor = 0.2f;
+    public float slowMotionDuration = 5f;
+    public Camera playerCamera; 
+    public float zoomedSize = 9f; 
+    public float zoomSpeed = 10f; 
 
-    private float normalTimeScale = 1f; 
-    private bool isSlowMotionActive = false; 
-    private float slowMotionTimer = 0f; 
+    private float normalTimeScale = 1f;
+    private bool isSlowMotionActive = false;
+    private float slowMotionTimer = 0f;
+    private float defaultSize; 
+
+    void Start()
+    {
+        
+        if (!playerCamera) playerCamera = Camera.main;
+
+        
+        defaultSize = playerCamera.orthographicSize;
+    }
 
     void Update()
     {
-        
         if (Input.GetMouseButtonDown(1) && !isSlowMotionActive)
         {
             ActivateSlowMotion();
         }
 
-        
         if ((Input.GetMouseButtonUp(1) || slowMotionTimer <= 0) && isSlowMotionActive)
         {
             DeactivateSlowMotion();
@@ -28,12 +39,22 @@ public class SlowMotionAbility : MonoBehaviour
         {
             slowMotionTimer -= Time.unscaledDeltaTime;
         }
+
+        //using lerf function math is cool
+        if (isSlowMotionActive)
+        {
+            playerCamera.orthographicSize = Mathf.Lerp(playerCamera.orthographicSize, zoomedSize, Time.unscaledDeltaTime * zoomSpeed);
+        }
+        else
+        {
+            playerCamera.orthographicSize = Mathf.Lerp(playerCamera.orthographicSize, defaultSize, Time.unscaledDeltaTime * zoomSpeed);
+        }
     }
 
     private void ActivateSlowMotion()
     {
         Time.timeScale = slowMotionFactor;
-        Time.fixedDeltaTime = Time.timeScale * 0.02f; // Adjust fixedDeltaTime according to the new time scale
+        Time.fixedDeltaTime = Time.timeScale * 0.02f;
         slowMotionTimer = slowMotionDuration;
         isSlowMotionActive = true;
     }
@@ -41,7 +62,7 @@ public class SlowMotionAbility : MonoBehaviour
     private void DeactivateSlowMotion()
     {
         Time.timeScale = normalTimeScale;
-        Time.fixedDeltaTime = normalTimeScale * 0.02f; // Reset fixedDeltaTime to its default value
+        Time.fixedDeltaTime = normalTimeScale * 0.02f;
         isSlowMotionActive = false;
     }
 }
